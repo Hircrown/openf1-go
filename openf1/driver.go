5086547a5e42b89f7b3250612bb38bf5f6f24115
Hircrown <221099175+Hircrown@users.noreply.github.com>
2025-10-21 11:52:31 +0200

    added driver method

---
+++ openf1/driver.go
@@ -0,0 +1,49 @@
+package openf1
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/Hircrown/openf1-go/openf1/types"
+)
+
+const driverPath = "/drivers"
+
+// Driver retrieves driver data based on the given user filter.
+// Wrong filter parameter won't result in error but in an empty slice.
+// Excessive data requests may result in an error.
+func (c *Client) Driver(filter types.DriverFilter) ([]types.Driver, error) {
+	fullURL := createFullURL(filter, c, driverPath)
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	switch res.StatusCode {
+	case 200:
+		defer res.Body.Close()
+		var driver []types.Driver
+		err = json.NewDecoder(res.Body).Decode(&driver)
+		if err != nil {
+			return nil, err
+		}
+		return driver, nil
+	case 422:
+		fallthrough
+	default:
+		defer res.Body.Close()
+		var errMsg types.ErrorMessage
+		err := json.NewDecoder(res.Body).Decode(&errMsg)
+		if err != nil {
+			return nil, err
+		}
+		return nil, errors.New(errMsg.Detail)
+	}
+
+}
