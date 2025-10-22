efb48ffb98a1cb0a443c74377b2809248ad902d4
Hircrown <221099175+Hircrown@users.noreply.github.com>
2025-10-22 15:07:57 +0200

    DRY HTTP GET logic

--- openf1/telemetry.go
+++ openf1/telemetry.go
@@ -1,10 +1,6 @@
 package openf1
 
 import (
-	"encoding/json"
-	"errors"
-	"net/http"
-
 	"github.com/Hircrown/openf1-go/openf1/types"
 )
 
@@ -15,35 +11,5 @@ const telemetryPath = "/car_data"
 // Excessive data requests may result in an error.
 func (c *Client) Telemetry(filter types.CarDataFilter) ([]types.CarData, error) {
 	fullURL := createFullURL(filter, c, telemetryPath)
-	req, err := http.NewRequest("GET", fullURL, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	res, err := c.httpClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	switch res.StatusCode {
-	case 200:
-		defer res.Body.Close()
-		var telemetry []types.CarData
-		err = json.NewDecoder(res.Body).Decode(&telemetry)
-		if err != nil {
-			return nil, err
-		}
-		return telemetry, nil
-	case 422:
-		fallthrough
-	default:
-		defer res.Body.Close()
-		var errMsg types.ErrorMessage
-		err := json.NewDecoder(res.Body).Decode(&errMsg)
-		if err != nil {
-			return nil, err
-		}
-		return nil, errors.New(errMsg.Detail)
-	}
-
+	return doGet[types.CarData](c.httpClient, fullURL)
 }
