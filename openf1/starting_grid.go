0d5b046c3e06cd349e453f7e31446e423fa4ad78
Hircrown <221099175+Hircrown@users.noreply.github.com>
2025-10-23 20:58:36 +0200

    added a starting_grid method

--- openf1/starting_grid.go
+++ openf1/starting_grid.go
@@ -1,6 +1,8 @@
 package openf1
 
 import (
+	"strconv"
+
 	"github.com/Hircrown/openf1-go/openf1/types"
 )
 
@@ -14,4 +16,23 @@ const startingGridPath = "/starting_grid"
 func (c *Client) StartingGrid(filter types.StartingGridFilter) ([]types.StartingGrid, error) {
 	fullURL := createFullURL(filter, c, startingGridPath)
 	return doGet[types.StartingGrid](c.httpClient, fullURL)
+}
+
+// StartingGridGP returns the starting grid for a specific Grand Prix, including both sprint and classic races.
+func (c *Client) StartingGridGP(countryName, year string, isSprintRace bool) ([]types.StartingGrid, error) {
+	qualifyingType := "Qualifying"
+	if isSprintRace {
+		qualifyingType = "Sprint"
+	}
+	sessionKey, err := c.GetSessionKey(countryName, "Qualifying", qualifyingType, year)
+	if err != nil {
+		return nil, err
+	}
+	grid, err := c.StartingGrid(types.StartingGridFilter{
+		SessionKey: strconv.Itoa(sessionKey),
+	})
+	if err != nil {
+		return nil, err
+	}
+	return grid, nil
 }
